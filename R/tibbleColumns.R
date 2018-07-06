94f79981627e996f687ad43483dee7f25d106405
nhemerson <nathan_h_emerson@homedepot.com>
2018-07-06 13:28:59 -0700

    Update replace_all_na function
    added DF output to function that is not tbl_df(df). Current state does not output a DF. This is fixed.

--- R/tibbleColumns.R
+++ R/tibbleColumns.R
@@ -259,8 +259,10 @@ replace_all_na <- function(df,replace_with = NULL){
   
   if(is.null(replace_with)){
     df[is.na(df)] <- 0
+    df
   }else{
     df[is.na(df)] <- replace_with
+    df
   }
   
 }
